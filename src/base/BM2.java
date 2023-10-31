333255def293f67f844b1879615bd50c2b6138e9
s12171934 <ssong99999@naver.com>
2023-10-31 16:35:09 +0900

    Branch songjunhyung

--- src/base/BM2.java
+++ src/base/BM2.java
@@ -1,4 +1,76 @@
 package base;
 
-public class BM2 {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BM2 extends BookManager {
+    private ArrayList<Book> bookList = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    @Override
+    void init() {
+        bookList.add(new Book(1L, "돈의 속성(300쇄 리커버에디션)", "김승호", Long.parseLong("9791188331796"), LocalDate.parse("2020-06-15")));
+        bookList.add(new Book(2L,"K 배터리 레볼루션", "박순혁", Long.parseLong("9791191521221"), LocalDate.parse("2023-02-20")));
+        bookList.add(new Book(3L, "위기의 역사", "오건영", Long.parseLong("9791169850360"), LocalDate.parse("2023-07-19")));
+    }
+
+    @Override
+    void interactWithUser() {
+        while (true) {
+            System.out.println("■■■■■■ 도서 관리 프로그램 ■■■■■■");
+            System.out.println("(1) 도서 조회");
+            System.out.println("(2) 도서 등록");
+            System.out.println("(3) 도서 수정");
+            System.out.println("(4) 도서 삭제");
+            System.out.println("(q) 프로그램 종료");
+            System.out.print("선택 >> ");
+            String userInput = sc.nextLine();
+
+            switch (userInput) {
+                case "1":
+                    // 조회
+                    printAllBook();
+                    break;
+                case "2":
+                    // 등록
+                    addBook();
+                    break;
+                case "3":
+                    // 수정
+                    updateBook();
+                    break;
+                case "4":
+                    // 삭제
+                    removeBook();
+                    break;
+                case "q":
+                    // 메소드를 종료
+                    System.out.println("프로그램 종료!");
+                    return;
+            }
+
+        }
+
+    }
+
+    @Override
+    void addBook() {
+
+    }
+
+    @Override
+    void printAllBook() {
+
+    }
+
+    @Override
+    void updateBook() {
+
+    }
+
+    @Override
+    void removeBook() {
+
+    }
 }
